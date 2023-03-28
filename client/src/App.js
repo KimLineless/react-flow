85333400f67e19fae10abdee72bfc3860d22af37
lineless <2837599@naver.com>
2023-03-28 18:01:28 +0900

    flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,16 @@
 import "./App.css";
-import React, { useEffect, useState, useRef } from "react";
-import { EdgesFlow, initialNodes } from "./custom1";
+import React, { useEffect, useState } from "react";
+import { EdgesFlow } from "./custom1";
 import Axios from "axios";
 
 function App() {
   const [data, setData] = useState([]);
-  const [edge, setEdge] = useState([]);
-  const [net, setNet] = useState([]);
-  const [rou, setRou] = useState([]);
-  const [sensor, setSensor] = useState([]);
-  const [server, setServer] = useState([]);
   useEffect(() => {
     Axios.get("/api/allFacilInfo")
       .then(res => {
         if (res.status === 200) {
           console.log(res.data);
           setData(res.data);
-          setEdge(res.data?.edgeInfo);
-          setNet(
-            res.data?.networkInfo.sort(function (a, b) {
-              return a.rcv_no - b.rcv_no;
-            }),
-          );
-          setRou(
-            res.data?.routerInfo.sort(function (a, b) {
-              return a.rcv_no - b.rcv_no;
-            }),
-          );
-          setSensor(
-            res.data?.sensorInfo.sort(function (a, b) {
-              return a.rcv_no - b.rcv_no;
-            }),
-          );
-          setServer(
-            res.data?.serverInfo.sort(function (a, b) {
-              return a.rcv_no - b.rcv_no;
-            }),
-          );
         }
       })
       .catch(e => {
@@ -46,14 +20,7 @@ function App() {
 
   return (
     <div className="App" style={{ width: "100%", height: "100vh" }}>
-      <EdgesFlow
-        data={data}
-        edge={edge}
-        net={net}
-        rou={rou}
-        sen={sensor}
-        ser={server}
-      />
+      <EdgesFlow data={data} />
     </div>
   );
 }
--- client/src/custom1.js
+++ client/src/custom1.js
@@ -1,335 +1,61 @@
 import React, { useCallback, useState } from "react";
 import ReactFlow, { useNodesState, useEdgesState, addEdge } from "reactflow";
 import "reactflow/dist/style.css";
-import Cloud from "./img/CLOUDL4_1_D.png";
-import Gw from "./img/gw_d.png";
-import Vpn from "./img/vpn_d.png";
-import Switch from "./img/switch_d.png";
-import Server from "./img/server_d.png";
-import Sensor from "./img/sen_d.png";
-import Ap from "./img/ap_d.png";
+import Modal from "react-modal";
 import CustomEdge from "./CustomEdge";
-import vis from "vis";
-
-let initialNodes = [
-  {
-    id: "node-1",
-    type: "input",
-    data: { label: "ICT L4 switch" },
-    position: { x: 0, y: -92 },
-    style: getNodeStyle(Cloud),
-  },
-  {
-    id: "node-2",
-    data: { label: "GATEWAY" },
-    position: { x: 0, y: 100 },
-    sourcePosition: "right",
-    style: getNodeStyle(Gw, "50%,80%"),
-  },
-  {
-    id: "node-2a",
-    data: { label: "방화벽" },
-    position: { x: 120, y: 200 },
-    style: getNodeStyle(Vpn),
-  },
-  {
-    id: "node-2a-1",
-    data: { label: "l3 switch" },
-    position: { x: 120, y: 330 },
-    style: getNodeStyle(Switch, "50%,80%"),
-  },
-  {
-    id: "node-2a-2",
-    data: { label: "l2 switch" },
-    position: { x: 120, y: 450 },
-    style: getNodeStyle(Switch, "50%,80%"),
-  },
-  {
-    id: "node-2a-3",
-    data: { label: "server1" },
-    position: { x: 40, y: 650 },
-    style: getNodeStyle(Server),
-  },
-  {
-    id: "node-2a-4",
-    data: { label: "server2" },
-    position: { x: 200, y: 650 },
-    style: getNodeStyle(Server),
-  },
-  {
-    id: "node-3a",
-    data: { label: "AP1" },
-    position: { x: 500, y: -92 },
-    targetPosition: "left",
-    sourcePosition: "right",
-    style: getNodeStyle(Ap),
-  },
-  {
-    id: "node-3a-1",
-    data: { label: "센서1" },
-    position: { x: 700, y: -167 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-  {
-    id: "node-3a-2",
-    data: { label: "센서2" },
-    position: { x: 700, y: -77 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-  {
-    id: "node-3a-3",
-    data: { label: "sensor3" },
-    position: { x: 700, y: 17 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-  {
-    id: "node-3b",
-    data: { label: "AP2" },
-    position: { x: 500, y: 208 },
-    targetPosition: "left",
-    sourcePosition: "right",
-    style: getNodeStyle(Ap),
-  },
-  {
-    id: "node-3b-1",
-    data: { label: "sensor4" },
-    position: { x: 700, y: 133.4 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-  {
-    id: "node-3b-2",
-    data: { label: "sensor5" },
-    position: { x: 700, y: 223.4 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-  {
-    id: "node-3b-3",
-    data: { label: "sensor6" },
-    position: { x: 700, y: 313.4 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-  {
-    id: "node-3c",
-    data: { label: "AP3" },
-    position: { x: 500, y: 508 },
-    targetPosition: "left",
-    sourcePosition: "right",
-    style: getNodeStyle(Ap),
-  },
-  {
-    id: "node-3c-1",
-    data: { label: "sensor7" },
-    position: { x: 700, y: 433.3 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-  {
-    id: "node-3c-2",
-    data: { label: "sensor8" },
-    position: { x: 700, y: 523.3 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-  {
-    id: "node-3c-3",
-    data: { label: "sensor9" },
-    position: { x: 700, y: 613.3 },
-    targetPosition: "left",
-    style: getNodeStyle2(Sensor),
-  },
-];
-
-const edgeStyle = {
-  stroke: "#0AA596",
-};
-
-const initialEdges = [
-  {
-    id: "edge-1-2",
-    source: "node-1",
-    target: "node-2",
-    className: "normal-edge",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-2-2",
-    source: "node-2",
-    target: "node-2a",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-2-2a",
-    source: "node-2a",
-    target: "node-2a-1",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-2-2b",
-    source: "node-2a-1",
-    target: "node-2a-2",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-2-2c",
-    source: "node-2a-2",
-    target: "node-2a-3",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-2-2d",
-    source: "node-2a-2",
-    target: "node-2a-4",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-2-3",
-    source: "node-2",
-    target: "node-3a",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3-1",
-    source: "node-3a",
-    target: "node-3a-1",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3-2",
-    source: "node-3a",
-    target: "node-3a-2",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3-3",
-    source: "node-3a",
-    target: "node-3a-3",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-2-4",
-    source: "node-2",
-    target: "node-3b",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3b-1",
-    source: "node-3b",
-    target: "node-3b-1",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3b-2",
-    source: "node-3b",
-    target: "node-3b-2",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3b-3",
-    source: "node-3b",
-    target: "node-3b-3",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-2-5",
-    source: "node-2",
-    target: "node-3c",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3c-1",
-    source: "node-3c",
-    target: "node-3c-1",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3c-2",
-    source: "node-3c",
-    target: "node-3c-2",
-    type: "step",
-    style: edgeStyle,
-  },
-  {
-    id: "edge-3c-3",
-    source: "node-3c",
-    target: "node-3c-3",
-    type: "step",
-    style: edgeStyle,
-  },
-];
+import {
+  initialNodes,
+  getNodeStyle,
+  getNodeStyle2,
+} from "./components/Nodes.js";
+import initialEdges from "./components/Edges.js";
 
 const edgeTypes = {
   custom: CustomEdge,
 };
 
-function getNodeStyle(backgroundImage, backgroundPosition = "50%,50%") {
-  return {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "contain",
-    backgroundRepeat: "no-repeat",
-    width: 80,
-    height: 80,
-    padding: "10px",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    whiteSpace: "noWrap",
-    paddingTop: "7%",
-    fontWeight: "bold",
-    color: "#111",
-    border: "none",
-    backgroundPosition: backgroundPosition,
-  };
-}
-
-function getNodeStyle2(backgroundImage, backgroundPosition = "50%,50%") {
-  return {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "contain",
-    backgroundRepeat: "no-repeat",
-    whiteSpace: "noWrap",
-    width: 50,
-    height: 50,
-    padding: "10px",
-    display: "flex",
-    paddingTop: "5%",
-    alignItems: "center",
-    justifyContent: "center",
-    fontWeight: "bold",
-    color: "#111",
-    border: "none",
-    backgroundPosition: backgroundPosition,
-  };
-}
-
 const EdgesFlow = props => {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [nodeToRemove, setNodeToRemove] = useState(null);
 
   const onConnect = useCallback(
     params => setEdges(eds => addEdge(params, eds)),
     [],
   );
+
+  const removeNodeAndDescendants = nodeId => {
+    setNodes(nodes => nodes.filter(n => n.id !== nodeId));
+    setEdges(edges =>
+      edges.filter(e => e.source !== nodeId && e.target !== nodeId),
+    );
+    const children = edges.filter(e => e.source === nodeId).map(e => e.target);
+    children.forEach(childId => {
+      removeNodeAndDescendants(childId);
+    });
+  };
+
+  const handleNodeContextMenu = (event, node) => {
+    event.preventDefault();
+    setNodeToRemove(node.id);
+    setModalOpen(true);
+  };
+
+  const handleNodeRemoveConfirm = () => {
+    // 삭제 대상 노드와 연결된 edge들을 찾아서 삭제
+    const connectedEdges = edges.filter(edge => {
+      return edge.source === nodeToRemove || edge.target === nodeToRemove;
+    });
+    const updatedEdges = edges.filter(edge => !connectedEdges.includes(edge));
+
+    // 삭제 대상 노드와 자식 노드들을 찾아서 삭제
+    removeNodeAndDescendants(nodeToRemove);
+
+    setEdges(updatedEdges);
+    setNodeToRemove(null);
+    setModalOpen(false);
+  };
 
   return (
     <>
@@ -343,9 +69,33 @@ const EdgesFlow = props => {
         edgeTypes={edgeTypes}
         fitView
         elements={initialNodes}
+        onNodeContextMenu={handleNodeContextMenu}
       ></ReactFlow>
+      <Modal
+        isOpen={modalOpen}
+        style={{
+          content: {
+            width: "400px",
+            height: "200px",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        }}
+      >
+        <h2>이 노드를 삭제하시겠습니까?</h2>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <button onClick={handleNodeRemoveConfirm}>네</button>
+          <button onClick={() => setModalOpen(false)}>아니요</button>
+        </div>
+      </Modal>
     </>
   );
 };
 
-export { EdgesFlow, initialNodes, initialEdges };
+export { EdgesFlow };
